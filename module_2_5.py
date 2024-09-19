079f764fa475d2411c0ce6435a4a1b548d6d5dd1
6u6lik16 <serega22222223@mail.ru>
2024-09-19 17:31:50 +0400

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,12 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+
+    return matrix
+
+mat = get_matrix(1,2, 13)
+print(mat)
